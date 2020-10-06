0fa5209aa0fd22ad5ada6457b16016fc565b978a
Mamadou Camara <camara0388@gmail.com>
2020-10-07 01:21:19 +0200

    train classifier model using pipeline

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,142 @@
 import sys
+import numpy as np
+import pandas as pd
+import re
+import pickle
+import time
+
+import nltk
+from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+nltk.download(['punkt', 'stopwords', 'wordnet', 'averaged_perceptron_tagger'])
+
+from sqlalchemy import create_engine
+
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import f1_score, classification_report
+
+
+class StartingVerbExtractor(BaseEstimator, TransformerMixin):
+    """Create new transform which check
+     it a word is a starting verb
+    """
+    def starting_verb(self, text):
+        sentence_list = nltk.sent_tokenize(text)
+        for sentence in sentence_list:
+            pos_tags = nltk.pos_tag(tokenize(sentence))
+            if len(pos_tags) > 0:
+                first_word, first_tag = pos_tags[0]
+                if first_tag in ['VB', 'VBP'] or first_word == 'RT':
+                    return 1
+        return 0
+
+    def fit(self, x, y=None):
+        return self
+
+    def transform(self, X):
+        X_tagged = pd.Series(X).apply(self.starting_verb)
+        return pd.DataFrame(X_tagged)
 
 
 def load_data(database_filepath):
-    pass
+    """Load data from database file
+    :param database_filepath: path to database
+    :return: tuple of X, y matrix and categories
+    name
+    meaning X is matrix of features and y
+    labels, categories name
+    """
+    # load data from database
+    engine = create_engine('sqlite:///'+database_filepath)
+    df = pd.read_sql_table("DisasterTable", con=engine)
+    X = df.message
+    y = df.iloc[:, 4:]
+    cat_names = y.columns.tolist()
+    return X, y, cat_names
 
 
 def tokenize(text):
-    pass
+    """Tokenize a text meaning
+    bag of word, remove punctuation and stop
+    words, make it lower and lemmatize
+
+    :param text: text to tokenize
+    :return: clean toknized text
+    """
+    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]' + \
+                 '|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    detected_urls = re.findall(url_regex, text)
+    for url in detected_urls:
+        text = text.replace(url, "urlplaceholder")
+    word_text = re.sub(r"[^a-zA-Z0-9]", " ", text)
+    tokens = word_tokenize(word_text)
+    word_tokens = [word for word in tokens if
+                   word not in stopwords.words("english")]
+    lemmatizer = WordNetLemmatizer()
+    clean_tokens = []
+    for tok in word_tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
 
 
 def build_model():
-    pass
+    """
+    Build Adabost model by using pipeline
+    and Gridsearch
+    :return:
+    """
+    pipeline = Pipeline([
+        ('features', FeatureUnion([
+
+            ('text_pipeline', Pipeline([
+                ('vect', CountVectorizer(tokenizer=tokenize)),
+                ('tfidf', TfidfTransformer())
+            ])),
+
+            ('starting_verb', StartingVerbExtractor())
+        ])),
+
+        ('aclf', MultiOutputClassifier(AdaBoostClassifier()))
+    ])
+    parameters = {
+        'aclf__estimator__learning_rate': [0.1, 0.2, 0.3],
+        'aclf__estimator__n_estimators': [100, 200]
+    }
+    cv = GridSearchCV(estimator=pipeline, param_grid=parameters, cv=3,
+                      n_jobs=-1, verbose=3)
+    return cv
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    """Evaluate the trainedmodel
+    :param model: estimator
+    :param X_test: disaster message to test
+    :param Y_test: disaster label
+    :param category_names: categories of the label
+    :return: print classification of each  categories
+    """
+    y_prediction = model.predict(X_test)
+    print(classification_report(Y_test.iloc[:, 1:].values,
+                                np.array([x[1:] for x in y_prediction]),
+                                target_names=category_names))
 
 
 def save_model(model, model_filepath):
-    pass
+    """Save the model with pickle
+    :param model: estimator
+    :param model_filepath: path file to save the model
+    :return: None
+    """
+    with open(model_filepath, 'wb') as file:
+        pickle.dump(model, file)
 
 
 def main():
@@ -27,13 +145,13 @@ def main():
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
         X, Y, category_names = load_data(database_filepath)
         X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
-        
+
         print('Building model...')
         model = build_model()
-        
+
         print('Training model...')
         model.fit(X_train, Y_train)
-        
+
         print('Evaluating model...')
         evaluate_model(model, X_test, Y_test, category_names)
 
